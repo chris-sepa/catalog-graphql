f481ffda0b453e208d566b6e46cfd6a23088036e
chris.sweeney <chris.sweeney@sepa.org.uk>
2020-01-28 10:09:44 +0000

    add root resolver

--- server.js
+++ server.js
@@ -2,24 +2,25 @@ require('graphql');
 require('apollo-server');
 import schema from './schema';
 
+//root resolver should return the Catalog for <http://data.sepa.org.uk/id/dcat/catalog/sewebCatalog>
 const resolvers = {
     Query: {
-      catalog(parent, args, context, info) {
-        return {catalog: "A catalog!"};
-      },
+        catalog(parent, args, context, info) {
+            return { catalog: "http://data.sepa.org.uk/id/dcat/catalog/sewebCatalog" };
+        },
     },
     Dataset: {
-      datasets(catalog) {
-        return {datasets: "datasets"};
-      },
+        datasets(catalog) {
+            return { datasets: "datasets" };
+        },
     },
-  };
+};
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-  });
-  
-  server.listen().then(({ url }) => {
+});
+
+server.listen().then(({ url }) => {
     console.log(`🚀 Server ready at ${url}`);
-  });
+});
