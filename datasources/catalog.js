e0d23f753a92c7c559173761a4f3897f71fa997e
chris.sweeney <chris.sweeney@sepa.org.uk>
2020-01-30 15:31:29 +0000

    start of datasources, not yet hooked to resolvers

--- datasources/catalog.js
+++ datasources/catalog.js
@@ -1,8 +1,15 @@
+//Try using SOH to bring catalog graph
+// and individual datasets with their distributions
 const { RESTDataSource } = require('apollo-datasource-rest');
+const updateEndpoint = 'update';
+const queryEndpoint = 'sparql';
 class catalogAPI extends RESTDataSource {
     constructor() {
         super();
-        this.baseURL = 'http://sepa-app-lds03:3030/sepadcat/query';
+        this.baseURL = 'http://sepa-app-lds03:3030/sepadcat/';
+    }
+    async getAllDatasets() {
+
     }
 }
 
